b402401017be402420ec8104ff815bcc023c9b82
minho60 <minho031@gmail.com>
2025-12-17 20:55:07 +0900

    1217 메인 페이지 변경

--- assets/js/main/sale-hover.js
+++ assets/js/main/sale-hover.js
@@ -18,3 +18,21 @@ lis.forEach(li => {
     img.src = originalSrc;
   });
 });
+
+const reco = document.querySelectorAll('.minho-wrapper .reco-left ul li');
+
+reco.forEach(li => {
+  const img = li.querySelector('img');
+  const originalSrc = img.src;
+  // hover용 이미지 경로를 data-hover 속성에서 가져오기
+  const hoverSrc = img.getAttribute('reco-data');
+
+  li.addEventListener("mouseover", () => {
+     img.src = hoverSrc;
+  });
+
+  li.addEventListener("mouseout", () => {
+    img.src = originalSrc;
+  });
+});
+ 
