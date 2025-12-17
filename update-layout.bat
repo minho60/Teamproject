08fc34903b8e55e7b4d601d298822bcf66e884ee
minho60 <minho031@gmail.com>
2025-12-17 16:46:46 +0900

    1217

--- update-layout.bat
+++ update-layout.bat
@@ -1,23 +1,20 @@
 @echo off
-chcp 65001 > nul
 
 echo ==================================================
-echo [1단계] "dodram-all" Git 저장소 다운로드 중..
+echo 1
 echo ==================================================
-:: 만약 이전에 받다 만 폴더가 있으면 지우고 다시 받기
 if exist "dodram-all" rmdir /s /q "dodram-all"
 git clone https://github.com/kahyou22/dodram-all.git
 
 echo.
 echo ==================================================
-echo [2단계] 파일 복사 중..
+echo 2
 echo ==================================================
 
 if not exist "assets\css" mkdir "assets\css"
 if not exist "assets\js" mkdir "assets\js"
 if not exist "assets\img" mkdir "assets\img"
 
-:: 'assets' 폴더 가져오기 (덮어씀)
 @REM xcopy "dodram-all\assets" "assets" /E /H /Y /I
 xcopy "dodram-all\assets\img\layout" "assets\img\layout" /E /H /Y /I
 
@@ -27,10 +24,10 @@ copy /Y "dodram-all\assets\js\layout.js" "assets\js\"
 
 echo.
 echo ==================================================
-echo [3단계] 임시 폴더 삭제 중..
+echo 3
 echo ==================================================
 rmdir /s /q "dodram-all"
 
 echo.
-echo [완료] 업데이트가 완료되었습니다.
+echo OK.
 pause
